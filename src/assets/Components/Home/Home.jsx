b847ff579c1e1bd8743f305406af69d6b2e82da8
mosesmbuthia <mbuthiamoses435@gmail.com>
2025-03-13 16:38:53 +0300

    performed the project setup

---
+++ src/assets/Components/Home/Home.jsx
@@ -0,0 +1,18 @@
+
+import "./Home.css"
+function Home() {
+  return (
+    <div className='home-page'>
+        <div className="home">
+            <h1>nato phonetic alpabet</h1>
+            <p>convert text to <a href="https://en.wikipedia.org/wiki/NATO_phonetic_alphabet">nato phonetic alphabet</a></p>
+            <div className="input-form">
+                <input type="text" />
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Home
